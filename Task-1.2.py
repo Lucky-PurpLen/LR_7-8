1cf128ca2fe221b508e6d785f96256e85096940d
UnluckyArtem <98209956+Lucky-PurpLen@users.noreply.github.com>
2025-11-06 16:02:22 +0300

    Add files via upload

---
+++ Task-1.2.py
@@ -0,0 +1,3 @@
+string = input('Введите строку ') + ", "
+print(string * 2 + string[:-2])
+print("Количество символов в строке - ", len(string) - 2)
---
+++ task_1_2.pas
@@ -0,0 +1,18 @@
+﻿program task_1_2;
+
+var
+  str:string;
+  i:integer;
+  
+begin
+  writeln('Введите строку');
+  readln(str);
+  for i:= 1 to 3 do
+  begin
+    write(str);
+    if i < 3 then
+      write(', ');    
+  end;
+  writeln();
+  writeln('Количество символов в строке - ',Length(str))
+end.
