21893a37816cfd4a2762b3b7615b71fb7091fb92
UnluckyArtem <98209956+Lucky-PurpLen@users.noreply.github.com>
2025-11-13 15:46:34 +0300

    Add files via upload

---
+++ task_2_2.pas
@@ -0,0 +1,19 @@
+﻿program task_2_2;
+
+var
+  str:string;
+  posX,posW:integer;
+  
+begin
+  writeln('Введите строку');
+  readln(str);
+  posX:= Pos('x',str);
+  posW:= Pos('w',str);
+  if (posW = 0) or (posX = 0) then
+    writeln('Нет таких символов')
+  else
+    if posX < posW then
+      writeln('Первее X')
+    else
+      writeln('Первее W');
+end.
